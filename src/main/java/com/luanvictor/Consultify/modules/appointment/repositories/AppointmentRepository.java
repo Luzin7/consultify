678146093f3ec0d7fe9e8a3394cfcc97f9226b22
Luzin7 <lvictor.dutra@gmail.com>
2024-08-24 21:09:42 -0300

    fix: adjust import

--- src/main/java/com/luanvictor/Consultify/modules/appointment/repositories/AppointmentRepository.java
+++ src/main/java/com/luanvictor/Consultify/modules/appointment/repositories/AppointmentRepository.java
@@ -1,6 +1,6 @@
-package com.luanvictor.Consultify.modules.inviteToken.repositories;
+package com.luanvictor.Consultify.modules.appointment.repositories;
 
-import com.luanvictor.Consultify.modules.inviteToken.entities.Appointment;
+import com.luanvictor.Consultify.modules.appointment.entities.Appointment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
--- src/main/java/com/luanvictor/Consultify/modules/inviteToken/repositories/InviteTokenRepository.java
+++ src/main/java/com/luanvictor/Consultify/modules/inviteToken/repositories/InviteTokenRepository.java
@@ -1,6 +1,6 @@
-package com.luanvictor.Consultify.modules.appointment.repositories;
+package com.luanvictor.Consultify.modules.inviteToken.repositories;
 
-import com.luanvictor.Consultify.modules.appointment.entities.InviteToken;
+import com.luanvictor.Consultify.modules.inviteToken.entities.InviteToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface InviteTokenRepository extends JpaRepository<InviteToken, String> {
