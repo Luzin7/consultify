0f26a894be1f8790446fa4fbe6a4dc4797047a01
Luzin7 <lvictor.dutra@gmail.com>
2024-08-24 21:20:25 -0300

    chore: under develop

--- src/main/java/com/luanvictor/Consultify/modules/admin/controllers/HelloWorld.java
+++ src/main/java/com/luanvictor/Consultify/modules/admin/controllers/HelloWorld.java
@@ -1,22 +1,18 @@
 package com.luanvictor.Consultify.modules.admin.controllers;
 
-import com.luanvictor.Consultify.modules.admin.dto.RegisterRequestDTO;
-import com.luanvictor.Consultify.modules.admin.services.CreateAdminService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/admin/create")
-@ResponseStatus(HttpStatus.CREATED)
-@RequiredArgsConstructor
-public class CreateAdminController {
-    private final CreateAdminService createAdminService;
-
-    public void handle(@RequestBody RegisterRequestDTO body) {
-        this.createAdminService.execute(body);
+@RequestMapping("/admin")
+@ResponseStatus(HttpStatus.OK)
+public class HelloWorld {
+    @GetMapping("/hello")
+    public ResponseEntity<String> handle() {
+        return ResponseEntity.ok("Hello World");
     }
 }
--- src/main/java/com/luanvictor/Consultify/providers/decorators/refreshToken/RefreshToken.java
+++ src/main/java/com/luanvictor/Consultify/providers/decorators/refreshToken/RefreshToken.java
@@ -1,4 +1,11 @@
 package com.luanvictor.Consultify.providers.decorators.refreshToken;
 
-public interface RefreshToken {
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface RefreshToken {
 }
