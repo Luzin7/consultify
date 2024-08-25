840f850a1d864e48a31a4ccf42bb97e16a83e7d8
Luzin7 <lvictor.dutra@gmail.com>
2024-08-24 21:10:19 -0300

    feat: add auth token annotation

--- src/main/java/com/luanvictor/Consultify/providers/decorators/authToken/AuthToken.java
+++ src/main/java/com/luanvictor/Consultify/providers/decorators/authToken/AuthToken.java
@@ -1,4 +1,11 @@
 package com.luanvictor.Consultify.providers.decorators.authToken;
 
-public interface AuthToken {
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface AuthToken {
 }
--- src/main/java/com/luanvictor/Consultify/providers/decorators/authToken/AuthTokenArgumentResolver.java
+++ src/main/java/com/luanvictor/Consultify/providers/decorators/authToken/AuthTokenArgumentResolver.java
@@ -1,4 +1,28 @@
 package com.luanvictor.Consultify.providers.decorators.authToken;
 
-public class AuthTokenArgumentResolver {
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@Component
+public class AuthTokenArgumentResolver implements HandlerMethodArgumentResolver {
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.getParameterAnnotation(AuthToken.class) != null;
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
+            NativeWebRequest webRequest, org.springframework.web.bind.support.WebDataBinderFactory binderFactory) {
+        HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
+        String authHeader = request.getHeader("Authorization");
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            return null;
+        }
+        return authHeader.replace("Bearer ", "");
+    }
 }
