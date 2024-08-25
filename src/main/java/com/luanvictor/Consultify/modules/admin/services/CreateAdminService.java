186e641946bc2c6e2edade88723bedce5d5a9701
Luzin7 <lvictor.dutra@gmail.com>
2024-08-24 21:13:22 -0300

    feat: create admin service

--- src/main/java/com/luanvictor/Consultify/modules/admin/services/CreateAdminService.java
+++ src/main/java/com/luanvictor/Consultify/modules/admin/services/CreateAdminService.java
@@ -1,4 +1,33 @@
 package com.luanvictor.Consultify.modules.admin.services;
 
+import com.luanvictor.Consultify.modules.admin.dto.RegisterRequestDTO;
+import com.luanvictor.Consultify.modules.admin.entities.Admin;
+import com.luanvictor.Consultify.modules.admin.errors.EmailAlreadyInUseException;
+import com.luanvictor.Consultify.modules.admin.repositories.AdminRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
 public class CreateAdminService {
+    private final AdminRepository adminRepository;
+
+    public void execute(RegisterRequestDTO request) {
+        Optional<Admin> admin = this.adminRepository.findByEmail(request.email());
+
+        if (admin.isPresent()) {
+            throw new EmailAlreadyInUseException();
+        }
+
+        Admin newAdmin = new Admin();
+        newAdmin.setName(request.name());
+        newAdmin.setEmail(request.email());
+        newAdmin.setPassword(request.password()); // TODO: Hash the password
+        newAdmin.setCreatedAt(new Date());
+
+        this.adminRepository.save(newAdmin);
+    }
 }
