b9dc15bd00156b515757f9adcdf3672ce51b91dc
Luzin7 <lvictor.dutra@gmail.com>
2024-08-24 21:10:52 -0300

    chore: remove comments

--- src/main/java/com/luanvictor/Consultify/modules/admin/errors/EmailAlreadyInUseException.java
+++ src/main/java/com/luanvictor/Consultify/modules/admin/errors/EmailAlreadyInUseException.java
@@ -3,7 +3,7 @@ package com.luanvictor.Consultify.modules.admin.errors;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.CONFLICT)  // Define o status HTTP 409 Conflict
+@ResponseStatus(HttpStatus.CONFLICT)
 public class EmailAlreadyInUseException extends RuntimeException {
     public EmailAlreadyInUseException() {
         super("Email already in use");
