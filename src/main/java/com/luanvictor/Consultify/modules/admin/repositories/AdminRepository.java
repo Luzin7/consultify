4d6cb7e4c503ba64725156101f99382dbe93cfbe
Luzin7 <lvictor.dutra@gmail.com>
2024-08-24 21:09:24 -0300

    feat: add method

--- src/main/java/com/luanvictor/Consultify/modules/admin/repositories/AdminRepository.java
+++ src/main/java/com/luanvictor/Consultify/modules/admin/repositories/AdminRepository.java
@@ -3,5 +3,8 @@ package com.luanvictor.Consultify.modules.admin.repositories;
 import com.luanvictor.Consultify.modules.admin.entities.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AdminRepository extends JpaRepository<Admin, Long> {
+    Optional<Admin> findByEmail(String email);
 }
