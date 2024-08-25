225f76d282f42b9d2a60a4c36088bfcaf58e9219
Luzin7 <lvictor.dutra@gmail.com>
2024-08-24 21:12:32 -0300

    feat: add web config

--- src/main/java/com/luanvictor/Consultify/config/WebConfig.java
+++ src/main/java/com/luanvictor/Consultify/config/WebConfig.java
@@ -1,4 +1,23 @@
 package com.luanvictor.Consultify.config;
 
-public class WebConfig {
+import com.luanvictor.Consultify.providers.decorators.authToken.AuthTokenArgumentResolver;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    private final AuthTokenArgumentResolver authTokenArgumentResolver;
+
+    public WebConfig(AuthTokenArgumentResolver authTokenArgumentResolver) {
+        this.authTokenArgumentResolver = authTokenArgumentResolver;
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(authTokenArgumentResolver);
+    }
 }
