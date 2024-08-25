3a2032991e8b25cb3c44b4ebdd6f61d4bd737140
Luzin7 <lvictor.dutra@gmail.com>
2024-08-24 21:13:11 -0300

    feat: create create admin controller

--- src/main/java/com/luanvictor/Consultify/modules/admin/controllers/CreateAdminController.java
+++ src/main/java/com/luanvictor/Consultify/modules/admin/controllers/CreateAdminController.java
@@ -1,14 +1,20 @@
 package com.luanvictor.Consultify.modules.admin.controllers;
 
-import com.luanvictor.Consultify.providers.decorators.authToken.AuthToken;
+import com.luanvictor.Consultify.modules.admin.dto.RegisterRequestDTO;
+import com.luanvictor.Consultify.modules.admin.services.CreateAdminService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/admin")
+@ResponseStatus(HttpStatus.CREATED)
 @RequiredArgsConstructor
 public class CreateAdminController {
-    public ResponseEntity<String> Teste(@AuthToken String authToken) {
-        return ResponseEntity.ok(authToken);
+    private final CreateAdminService createAdminService;
+
+    @PostMapping("/create")
+    public void handle(@RequestBody RegisterRequestDTO body) {
+        this.createAdminService.execute(body);
     }
 }
