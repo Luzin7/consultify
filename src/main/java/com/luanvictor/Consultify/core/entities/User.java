7d1488718a1036601d36363b9a251482a55b423c
Luzin7 <lvictor.dutra@gmail.com>
2024-08-26 19:37:06 -0300

    feat: add base entity

---
+++ src/main/java/com/luanvictor/Consultify/core/entities/User.java
@@ -0,0 +1,35 @@
+package com.luanvictor.Consultify.core.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@MappedSuperclass
+public abstract class User extends BaseEntity {
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_at", updatable = false)
+    private Date createdAt;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "updated_at")
+    private Date updatedAt;
+
+    private String email;
+
+    private String password;
+
+    private String name;
+
+    private String role;
+}
