8583d685b089c2d2d9caeb26d4d94bec3d8f896b
Luzin7 <lvictor.dutra@gmail.com>
2024-08-26 19:39:13 -0300

    feat: create bcrypt services

---
+++ src/main/java/com/luanvictor/Consultify/providers/cryptography/PasswordEncryption.java
@@ -0,0 +1,19 @@
+package com.luanvictor.Consultify.providers.cryptography;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PasswordEncryption {
+    private final PasswordEncoder passwordEncoder;
+
+    public String encryptPassword(String rawPassword) {
+        return passwordEncoder.encode(rawPassword);
+    }
+
+    public boolean checkPassword(String rawPassword, String encodedPassword) {
+        return passwordEncoder.matches(rawPassword, encodedPassword);
+    }
+}
