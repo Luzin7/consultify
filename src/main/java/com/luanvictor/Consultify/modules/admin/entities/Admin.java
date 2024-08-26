d9d190600687d6cc99810774a75b1e3515fbf18e
Luzin7 <lvictor.dutra@gmail.com>
2024-08-26 19:40:36 -0300

    refactor: extend superclass

--- src/main/java/com/luanvictor/Consultify/modules/admin/entities/Admin.java
+++ src/main/java/com/luanvictor/Consultify/modules/admin/entities/Admin.java
@@ -1,35 +1,11 @@
 package com.luanvictor.Consultify.modules.admin.entities;
 
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-import java.util.Date;
+import com.luanvictor.Consultify.core.entities.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "administrators")
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-public class Admin {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+public class Admin extends User {
 
-    @Temporal(TemporalType.TIMESTAMP)
-    @Column(name = "created_at", updatable = false)
-    private Date createdAt;
-
-    @Temporal(TemporalType.TIMESTAMP)
-    @Column(name = "updated_at")
-    private Date updatedAt;
-
-    private String name;
-
-    private String email;
-
-    private String password;
 }
