a60b301007f7d22631a90a37cf15763191bbcbf0
Luzin7 <lvictor.dutra@gmail.com>
2024-08-31 18:52:48 -0300

    feat: add find by email

--- src/main/java/com/luanvictor/Consultify/modules/professional/repositories/ProfessionalRepository.java
+++ src/main/java/com/luanvictor/Consultify/modules/professional/repositories/ProfessionalRepository.java
@@ -3,5 +3,8 @@ package com.luanvictor.Consultify.modules.professional.repositories;
 import com.luanvictor.Consultify.modules.professional.entities.Professional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ProfessionalRepository extends JpaRepository<Professional, Long> {
+    Optional<Professional> findByEmail(String email);
 }
