380d763aac49a98a3b7b1d78690aae69a57a7f30
Luzin7 <lvictor.dutra@gmail.com>
2024-08-31 15:40:15 -0300

    feat: create controller

---
+++ src/main/java/com/luanvictor/Consultify/modules/admin/controllers/GetAdminDetailsByIdController.java
@@ -0,0 +1,25 @@
+package com.luanvictor.Consultify.modules.admin.controllers;
+
+import com.luanvictor.Consultify.modules.admin.dto.AuthRequestDTO;
+import com.luanvictor.Consultify.modules.admin.entities.Admin;
+import com.luanvictor.Consultify.modules.admin.services.GetAdminDetailsByIdService;
+import com.luanvictor.Consultify.providers.decorators.authToken.AuthToken;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/admin")
+@RequiredArgsConstructor
+public class GetAdminDetailsByIdController {
+    private final GetAdminDetailsByIdService getAdminDetailsById;
+
+    @PostMapping("/get-by-id")
+    public ResponseEntity<Admin> handle(@AuthToken AuthRequestDTO body) {
+        Admin result = this.getAdminDetailsById.execute(body);
+
+        return ResponseEntity.ok(result);
+    }
+}
