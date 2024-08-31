e1b79f774c2d12c1ce0ccdad2f219a1a50189d1f
Luzin7 <lvictor.dutra@gmail.com>
2024-08-31 15:43:20 -0300

    refactor: add dto

--- src/main/java/com/luanvictor/Consultify/modules/admin/controllers/LoginAdminController.java
+++ src/main/java/com/luanvictor/Consultify/modules/admin/controllers/LoginAdminController.java
@@ -1,22 +1,24 @@
 package com.luanvictor.Consultify.modules.admin.controllers;
 
 import com.luanvictor.Consultify.modules.admin.dto.LoginRequestDTO;
+import com.luanvictor.Consultify.modules.admin.dto.LoginResponseDTO;
 import com.luanvictor.Consultify.modules.admin.services.LoginAdminService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/admin")
-@ResponseStatus(HttpStatus.OK)
 @RequiredArgsConstructor
 public class LoginAdminController {
     private final LoginAdminService loginAdminService;
 
     @PostMapping("/login")
-    public ResponseEntity<String> handle(@RequestBody LoginRequestDTO body) {
-        String result = this.loginAdminService.execute(body);
+    public ResponseEntity<LoginResponseDTO> handle(@RequestBody LoginRequestDTO body) {
+        LoginResponseDTO result = this.loginAdminService.execute(body);
 
         return ResponseEntity.ok(result);
     }
