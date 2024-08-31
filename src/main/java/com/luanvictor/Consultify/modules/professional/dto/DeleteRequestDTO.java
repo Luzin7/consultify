3d323b61c99acd631c1eef834a666577ed241127
Luzin7 <lvictor.dutra@gmail.com>
2024-08-31 18:53:29 -0300

    feat: create dto

---
+++ src/main/java/com/luanvictor/Consultify/modules/professional/dto/DeleteRequestDTO.java
@@ -0,0 +1,4 @@
+package com.luanvictor.Consultify.modules.professional.dto;
+
+public record DeleteRequestDTO(String professionalId) {
+}
---
+++ src/main/java/com/luanvictor/Consultify/modules/professional/dto/RegisterRequestDTO.java
@@ -0,0 +1,4 @@
+package com.luanvictor.Consultify.modules.professional.dto;
+
+public record RegisterRequestDTO(String name, String email, String password) {
+}
