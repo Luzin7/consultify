d48b9003ce3652327a5ec68e1af5327ef900765f
Luzin7 <lvictor.dutra@gmail.com>
2024-10-21 17:39:14 -0300

    feat: implement method

--- src/main/java/com/luanvictor/Consultify/modules/inviteToken/repositories/InviteTokenRepository.java
+++ src/main/java/com/luanvictor/Consultify/modules/inviteToken/repositories/InviteTokenRepository.java
@@ -3,5 +3,8 @@ package com.luanvictor.Consultify.modules.inviteToken.repositories;
 import com.luanvictor.Consultify.modules.inviteToken.entities.InviteToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface InviteTokenRepository extends JpaRepository<InviteToken, String> {
+    Optional<InviteToken> findByToken(String token);
 }
